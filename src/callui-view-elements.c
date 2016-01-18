a1fd31e31ac80beef61932ffbd563c801e54d66a
Igor Olshevskyi <i.olshevskyi@samsung.com>
2016-01-18 13:11:40 +0200

    [Call UI] Change log format: add file name to display in logs.
    Change-Id: I59d695036ac4d6867afa8fa696ea1a63d777021f

--- src/callui-view-elements.c
+++ src/callui-view-elements.c
@@ -152,13 +152,13 @@ static void __callui_spk_btn_cb(void *data, Evas_Object *obj, void *event_info)
 	if (ad->speaker_status == EINA_TRUE) {
 		ret = cm_speaker_off(ad->cm_handle);
 		if (ret != CM_ERROR_NONE) {
-			err("cm_speaker_off() is failed");
+			err("cm_speaker_off() is failed. ret[%d]", ret);
 			return;
 		}
 	} else {
 		ret = cm_speaker_on(ad->cm_handle);
 		if (ret != CM_ERROR_NONE) {
-			err("cm_speaker_on() is failed");
+			err("cm_speaker_on() is failed. ret[%d]", ret);
 			return;
 		}
 	}
